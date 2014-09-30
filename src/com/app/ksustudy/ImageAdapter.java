f239dc454cb0cce6533570d3507210c5caf1e660
5LICK <slicknslick@yahoo.com>
2014-09-30 21:52:00 +0300

    Initial commit

---
+++ src/com/app/ksustudy/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.app.ksustudy;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+	private Context mContext;
+
+	public ImageAdapter(Context c) {
+		mContext = c;
+	}
+
+	public int getCount() {
+		return mThumbIds.length;
+	}
+
+	public Object getItem(int position) {
+		return mThumbIds[position];
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	// create a new ImageView for each item referenced by the Adapter
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageView;
+		if (convertView == null) {
+			// if it's not recycled, initialize some attributes
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(200, 260));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(3, 3, 3, 3);
+		} else {
+			imageView = (ImageView) convertView;
+		}
+
+		imageView.setImageResource(mThumbIds[position]);
+		return imageView;
+	}
+
+	// references to our images
+	public	Integer[] mThumbIds = { R.drawable.abs_ekonom_b, R.drawable.abs_fizmat, R.drawable.abs_kulimist,
+			R.drawable.abs_psiholog, R.drawable.abs_sport};
+
+}
---
+++ src/com/app/ksustudy/MainActivity.java
@@ -0,0 +1,57 @@
+package com.app.ksustudy;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+	GridView gvMain;
+	  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        gvMain = (GridView) findViewById(R.id.gridView1);
+        gvMain.setAdapter(new ImageAdapter(this));
+        
+        gvMain.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
